b3a95978d7c8dcc82713686867d5051f6e9351fe
Maikel Vlasman <maikel@mediamoose.nl>
2019-09-15 12:44:55 +0200

    Improve pytest command.

--- install.sh
+++ install.sh
@@ -6,15 +6,27 @@ rm -rf /usr/python-tools
 mv $(dirname $0)/python-tools /usr/python-tools
 rm -rf $(dirname $0)
 
-for file in $(ls "/usr/python-tools/project");do
-    if [ ! -e "$PWD/$file" ];then
-        ln -sf "/usr/python-tools/project/$file" "$PWD/$file"
-    fi
-done
-
 if [ "$1" != "" ] && [ -e "/usr/python-tools/$1/entrypoint.sh" ];then
     ln -sf /usr/python-tools/$1/entrypoint.sh /docker-entrypoint.sh
+else
+    ln -sf /usr/python-tools/entrypoint.sh /docker-entrypoint.sh
 fi
+
+if [ "$1" != "" ] && [ -d "/usr/python-tools/project/$1" ];then
+    for file in $(ls "/usr/python-tools/project/$1");do
+        if [ ! -e "$PWD/$file" ];then
+            ln -sf "/usr/python-tools/project/$1/$file" "$PWD/$file"
+        fi
+    done
+fi
+
+for file in $(ls "/usr/python-tools/project");do
+    if [ ! -e "$PWD/$file" ];then
+        if [ ! -e "$PWD/$file" ];then
+            ln -sf "/usr/python-tools/project/$file" "$PWD/$file"
+        fi
+    fi
+done
 
 if [ "$1" != "" ] && [ -d "/usr/python-tools/$1/bin" ];then
     for file in $(ls "/usr/python-tools/$1/bin");do
